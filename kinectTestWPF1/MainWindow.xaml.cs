30f611bb8ac647e8eba1b3bcf27c5d9603b0c7d3
haru2036 <hogehoge111@gmail.com>
2012-08-17 15:48:04 +0900

    First commit (3次元KaossPad的なもの）

---
+++ kinectTestWPF1/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.Wpf;
+using Midi;
+
+
+namespace kinectTestWPF1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+         KinectSensor kinect;
+         kinect2Midi midi1 = new kinect2Midi();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+         try {
+                if ( KinectSensor.KinectSensors.Count == 0 ) {
+                    throw new Exception( "Kinectが接続されていません" );
+                }
+
+                // Kinectインスタンスを取得する
+                kinect = KinectSensor.KinectSensors[0];
+
+                // すべてのフレーム更新通知をもらう
+                kinect.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>( kinect_AllFramesReady );
+
+                // Color,Depth,Skeletonを有効にする
+                kinect.ColorStream.Enable();
+                kinect.DepthStream.Enable();
+                kinect.SkeletonStream.Enable();
+
+                // Kinectの動作を開始する
+                kinect.Start();
+            }
+            catch ( Exception ex ) {
+                MessageBox.Show( ex.Message );
+                Close();
+            }
+        }
+
+        // すべてのデータの更新通知を受け取る
+        void kinect_AllFramesReady( object sender, AllFramesReadyEventArgs e )
+        {
+            imageRgbCamera.Source = e.OpenColorImageFrame().ToBitmapSource();
+            imageDepthCamera.Source = e.OpenDepthImageFrame().ToBitmapSource();
+
+            // 骨格位置の表示
+            ShowSkeleton( e );
+        }
+
+        private void ShowSkeleton( AllFramesReadyEventArgs e )
+        {
+            // キャンバスをクリアする
+            canvas1.Children.Clear();
+
+            // スケルトンフレームを取得する
+            SkeletonFrame skeletonFrame = e.OpenSkeletonFrame();
+            if ( skeletonFrame != null ) {
+                // スケルトンデータを取得する
+                Skeleton[] skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                skeletonFrame.CopySkeletonDataTo( skeletonData );
+
+                // プレーヤーごとのスケルトンを描画する
+                foreach ( var skeleton in skeletonData ) {
+                    if ( skeleton.TrackingState == SkeletonTrackingState.Tracked ) {
+                        // 骨格を描画する
+                        foreach (Joint joint in skeleton.Joints)
+                        {
+                            if (joint.JointType == JointType.HandRight)
+                            {
+                                midi1.sendAll(joint.Position.X, joint.Position.Y, joint.Position.Z);
+                                // 骨格の座標をカラー座標に変換する
+                                ColorImagePoint point = kinect.MapSkeletonPointToColor(joint.Position, kinect.ColorStream.Format);
+
+                                // 円を書く
+                                canvas1.Children.Add(new Ellipse()
+                                {
+                                    Margin = new Thickness(point.X, point.Y, 0, 0),
+                                    Fill = new SolidColorBrush(Colors.Red),
+                                    Width = 20,
+                                    Height = 20,
+                                });
+                            }
+                            else
+                            {
+                                // 骨格の座標をカラー座標に変換する
+                                ColorImagePoint point = kinect.MapSkeletonPointToColor(joint.Position, kinect.ColorStream.Format);
+
+                                // 円を書く
+                                canvas1.Children.Add(new Ellipse()
+                                {
+                                    Margin = new Thickness(point.X, point.Y, 0, 0),
+                                    Fill = new SolidColorBrush(Colors.Black),
+                                    Width = 20,
+                                    Height = 20,
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+       
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            midi1.sendX();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            midi1.sendY();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            midi1.sendZ();
+        }
+    }
+}
+
---
+++ kinectTestWPF1/kinect2Midi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Midi;
+namespace kinectTestWPF1
+{
+    class kinect2Midi
+    {
+        public OutputDevice outputDevice = OutputDevice.InstalledDevices[1];
+        int vx, vy, vz;
+        public kinect2Midi()
+        {
+            outputDevice.Open();
+        }
+        //実際に使うのはこっち
+        public void sendAll(float X, float Y, float Z)
+        {
+            vx = Math.Max(0,Math.Min(127, (int)(63.5 * (X + 1))));
+            vy = Math.Max(0, Math.Min(127, (int)(63.5 * (Y + 1))));
+            vz = Math.Max(0,Math.Min(127, (int)(127 * (Z-1))));
+            int cutoff, type, reso;
+            type = vz;
+            cutoff = vx;
+            reso = vy;
+
+
+            outputDevice.SendControlChange(Channel.Channel1, Midi.Control.ModulationWheel, cutoff);
+            outputDevice.SendControlChange(Channel.Channel2, Midi.Control.ModulationWheel, type);
+            outputDevice.SendControlChange(Channel.Channel3, Midi.Control.ModulationWheel, reso);
+        }
+        //ここからの３つはMidi Learnで覚えさせるためのテスト信号
+        public void sendX()
+        {
+            outputDevice.SendControlChange(Channel.Channel1, Midi.Control.ModulationWheel, 63);
+        }
+        public void sendY()
+        {
+            outputDevice.SendControlChange(Channel.Channel3, Midi.Control.ModulationWheel, 63);
+        }
+        public void sendZ()
+        {
+            outputDevice.SendControlChange(Channel.Channel2, Midi.Control.ModulationWheel, 63);
+        }
+        ~kinect2Midi()
+        {
+            outputDevice.Close();
+        }
+    }
+}
